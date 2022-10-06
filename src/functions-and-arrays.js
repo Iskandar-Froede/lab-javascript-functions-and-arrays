145810f227cdd19b3a5264c8e30c11885c016f9a
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-10-06 10:54:59 +0200

    iteration1 done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,19 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+
+
+    if (num1 > num2) {
+      console.log(num1);
+      return num1;
+    } else {
+      console.log(num2);
+      return num2;
+    } 
+  } 
+  
+  maxOfTwoNumbers(2, 5)
+  
+
 
 
 
